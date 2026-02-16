c22ffbcd5366abf1196b7be01860ab67c0935b6a
gomez1112 <54489633+gomez1112@users.noreply.github.com>
2026-02-16 14:18:00 -0500

    Initial Commit

---
+++ PentalWords/PentalWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PentalWordsApp.swift
+//  PentalWords
+//
+//  Created by Gerard Gomez on 2/16/26.
+//
+
+import SwiftUI
+
+@main
+struct PentalWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
